4160b125c0843dcd4b47467209d474c5b099b889
Np1311 <130040190+Np1311@users.noreply.github.com>
2023-10-04 18:15:41 +0700

    update

--- build_files.sh
+++ build_files.sh
@@ -1,8 +1,9 @@
 echo "BUILD START"
+export CACHE_VERSION=$(($CACHE_VERSION + 1))
+
 python3 -m venv venv
 source venv/bin/activate
 pip install --upgrade pip
-pip install -r requirements.txt
-
+pip install --cache-dir=$CACHE_DIR -r requirements.txt
 python manage.py collectstatic --noinput
 echo "BUILD END"
