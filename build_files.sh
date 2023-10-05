8cf661933a45a6b20543a50af573dac4481b9c30
Np1311 <130040190+Np1311@users.noreply.github.com>
2023-10-05 13:36:17 +0700

    build command update

--- build_files.sh
+++ build_files.sh
@@ -29,10 +29,26 @@ du -sh venv
 
 # Debugging: List installed packages and their sizes
 echo "Installed packages and their sizes:"
-pip freeze | xargs pip show | awk '/^Name:|^Size:/'
+pip freeze | xargs pip show | awk '/^Name:/ {name=$2} /^Size:/ {print name, $2}'
 
 # Debugging: Print the size of the deployment directory
 du -sh .
+
+# Debugging: Print the size of specific files or directories in the environment
+echo "Size of specific files or directories in the environment:"
+du -sh venv/bin
+du -sh venv/lib
+
+# Debugging: Print the contents of the environment directory
+echo "Contents of the environment directory:"
+ls -lh venv
+
+# Print the size of specific files or directories in the project
+echo "Size of specific files or directories in the project:"
+du -sh manage.py
+du -sh requirements.txt
+du -sh static
+du -sh templates
 
 # Deactivate the virtual environment
 deactivate
