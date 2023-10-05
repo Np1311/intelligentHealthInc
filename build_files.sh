62735dcfdf6482cfd4d29c7ae6a8bae29b187b65
Np1311 <130040190+Np1311@users.noreply.github.com>
2023-10-05 13:26:13 +0700

    vercel setup

--- build_files.sh
+++ build_files.sh
@@ -1,9 +1,15 @@
+#!/bin/bash
+
 echo "BUILD START"
-export CACHE_VERSION=$(($CACHE_VERSION + 1))
+
+# Remove existing virtual environment if it exists
+rm -rf venv
 
 python3 -m venv venv
 source venv/bin/activate
 pip install --upgrade pip
-pip install --cache-dir=$CACHE_DIR -r requirements.txt
-python manage.py collectstatic --noinput
+pip install -r requirements.txt || { echo 'Failed to install dependencies'; exit 1; }
+python manage.py collectstatic --noinput || { echo 'Failed to collect static files'; exit 1; }
+deactivate
+
 echo "BUILD END"
