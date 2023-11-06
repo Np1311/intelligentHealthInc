3ceebfe20e9436829f042033c4dc4f2aab6feac1
Np1311 <130040190+Np1311@users.noreply.github.com>
2023-11-06 17:31:50 +0800

    build command update

--- build.sh
+++ build.sh
@@ -9,6 +9,6 @@ pip install -r requirements.txt
 pip uninstall tensorflow
 pip uninstall azure-storage-blob
 
-python manage.py makemigrations --merge
+python manage.py makemigrations 
 python manage.py migrate
 python manage.py collectstatic --no-input
