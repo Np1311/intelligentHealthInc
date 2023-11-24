fc45256c223e44b3fbff256862808b726b542fd0
Np1311 <130040190+Np1311@users.noreply.github.com>
2023-11-24 13:48:43 +0800

    disabled the template name

--- radiologistDoctor/form.py
+++ radiologistDoctor/form.py
@@ -2,11 +2,14 @@ from django import forms
 from .models import findingsTemplate
 from medicalTech.models import *
 
+
 class FindingsTemplateForm(forms.ModelForm):
     class Meta:
         model = findingsTemplate
         fields = ['template_name', 'template']
-    template_name = forms.CharField(label = 'Template Name',initial='Covid-19')
+    template_name = forms.CharField(
+        label='Template Name', initial='Covid-19', disabled=True)
+
 
 class UpdateTemplateForm(forms.ModelForm):
     class Meta:
@@ -15,14 +18,15 @@ class UpdateTemplateForm(forms.ModelForm):
 
     def __init__(self, *args, **kwargs):
         super(UpdateTemplateForm, self).__init__(*args, **kwargs)
-        
+
         # Access the instance being updated
         instance = kwargs.get('instance')
-        
+
         # Fill in the initial values for the fields
         if instance:
             self.fields['template_name'].initial = instance.template_name
             self.fields['template'].initial = instance.template
+
 
 class ImageFindingsForm(forms.ModelForm):
     class Meta:
@@ -32,19 +36,19 @@ class ImageFindingsForm(forms.ModelForm):
     findings = forms.CharField(label='Findings', widget=forms.Textarea)
     impressions = forms.CharField(label='Impressions', initial='Normal')
 
-    def __init__(self, *args, predictions_value=None,data = None,initial_data = None,instance = None, **kwargs):
-        
+    def __init__(self, *args, predictions_value=None, data=None, initial_data=None, instance=None, **kwargs):
+
         super().__init__(*args, **kwargs)
-        
+
         if data:
             self.fields['examination'].initial = instance.examination
             self.fields['findings'].initial = instance.findings
             self.fields['impressions'].initial = instance.impressions
-        
+
         else:
             if predictions_value == 'Positive':
                 # Fetch the initial value from the database
-                
+
                 self.fields['findings'].initial = initial_data.get('findings')
-                self.fields['impressions'].initial = initial_data.get('impressions')
-                
+                self.fields['impressions'].initial = initial_data.get(
+                    'impressions')
